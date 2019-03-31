d7aa87d9399205432f5b1f21803ed94ba8b5d879
H_H-XD <hussamDe8@gmail.com>
2019-03-31 13:10:52 +0300

    Change to Beer

--- starter-code/99_BottlesOfBeer/main.js
+++ starter-code/99_BottlesOfBeer/main.js
@@ -5,13 +5,13 @@ var $fragment = $(document.createDocumentFragment());
 for (var $i = 99; $i >= 0; $i--) {
   var $li = $('<li></li>');
   if ($i == 0) {
-    $li.text("No more bottles of coke on the wall, no more bottles of coke. Go to the store and buy some more, 99 bottles of coke on the wall.");
+    $li.text("No more bottles of Beer on the wall, no more bottles of Beer. Go to the store and buy some more, 99 bottles of Beer on the wall.");
   }
   else if ($i == 1) {
-    $li.text($i+" bottle of coke on the wall, " + $i + " bottle of coke. Take one down and pass it around, "+ ($i-1) +" bottle of coke on the wall.");
+    $li.text($i+" bottle of Beer on the wall, " + $i + " bottle of Beer. Take one down and pass it around, "+ ($i-1) +" bottle of Beer on the wall.");
   }
   else{
-    $li.text($i+" bottles of coke on the wall, " + $i + " bottles of coke. Take one down and pass it around, "+ ($i-1) +" bottles of coke on the wall.");
+    $li.text($i+" bottles of Beer on the wall, " + $i + " bottles of Beer. Take one down and pass it around, "+ ($i-1) +" bottles of Beer on the wall.");
   }
   $fragment.append($li);
 };
