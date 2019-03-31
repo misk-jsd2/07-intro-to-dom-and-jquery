0701962c8218adbe2675aa630196b8dc509b4b0f
Omar Alokali <3o.1414@gmail.com>
2019-03-31 18:12:29 +0300

    My homework

--- starter-code/HW/Hw.js
+++ starter-code/HW/Hw.js
@@ -5,18 +5,20 @@ for (var i = 99; i >= 2; i--) {
 //  console.log(a)
   function myFunction() {
   var node = document.createElement("LI");
-  event.preventDefault();
+//  event.preventDefault();
   var textnode = document.createTextNode(a);
   node.appendChild(textnode);
   document.getElementById("Lyrics").appendChild(node);
 }
+myFunction();
 }
 b = 'Take one down and pass it around, no more bottles of coke on the wall.\nNo more bottles of coke on the wall, no more bottles of coke.\nGo to the store and buy some more, 99 bottles of coke on the wall.'
 
-  function myFunction() {
+  function myFunction1() {
   var node1 = document.createElement("LI");
-  event.preventDefault();
+  //event.preventDefault();
   var textnode1 = document.createTextNode(b);
-  node.appendChild(textnode1);
+  node1.appendChild(textnode1);
   document.getElementById("Lyrics").appendChild(node1);
 }
+myFunction1();
