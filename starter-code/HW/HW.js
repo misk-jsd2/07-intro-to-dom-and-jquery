0f9cef7cb54481bb2b7f61e6925abad34d071486
Sultan alqaedi <salqaedi@gmail.com>
2019-03-31 21:31:42 +0300

    SULTAN ALQAEDI

---
+++ starter-code/HW/HW.js
@@ -0,0 +1,130 @@
+window.onload = function()
+{
+  var main_heading = document.createElement("h1");
+  var heading_text = document.createTextNode(" 99 Bottles of Coke");
+  main_heading.appendChild(heading_text);
+  document.getElementById("header").appendChild(main_heading);
+       var campusesContainer = document.getElementById("songlist");
+       var songlist = campusesContainer.getElementsByTagName("li");
+
+
+  var x = "bottles";
+var num = 99;
+while (num > 1) {
+var songlist1 = (num + " " + x + " of coke on the wall, "+num + " " + x + " of coke,"+"Take one down and pass it around,"+ (num-1) + " " + x + " of coke on the wall.");
+ num = num - 1;
+ if (num > 0) {
+
+ }
+
+ else {
+ var songlist2 = ("No more " + x + " of coke on the wall.");
+}
+var x1 = "bottle";
+if (num <= 1) 
+{
+ var songlist3 = (num + " " + x1 + " of coke on the wall, "+num + " " + x1 + " of coke,"+"Take one down and pass it around,no more bottles of coke on the wall.");
+
+ }
+if (num == 0) 
+{
+ var songlist4 = ("No more bottles of coke on the wall, no more bottles of coke. Go to the store and buy some more"+ " 99 bottles of coke on the wall.");
+
+ddToList (x0);
+var songlist3 = addToList (li);
+var songlist1 = addToList ('songlist');
+
+ var newThing = newThingInput.value;
+    addToList(thingList, newThing);
+ }
+
+}
+/ var myp = document.createElement('p');
+// myptext = document.createTextNode('hello');
+
+
+
+S
+
+// myp.appendChild(myptext);
+// document.body.appendChild(myp);
+
+
+
+
+
+
+
+
+
+
+//  var x = document.getElementById(myp);
+// x.innerHTML = "MY HEROO";
+
+// mybottl = document.getElementById('songlist');
+//   i;
+// for (var i = 99 ; i >= document.ul.length; i--)
+// {
+// if ( (i > document.ul.length) && (i > 1)){
+// document.write(document.ul[i] +" bottles of coke on the wall, "+ document.ul[i] + " bottles of coke. Take one down and pass it around, "+ (i-1) +" bottles of coke on the wall.");
+// }
+// else if ( (i > document.ul.length) && (i == 1)){
+// console.log(i +" bottle of coke on the wall, " + i + " bottle of coke. Take one down and pass it around, no more bottles of coke on the wall.");
+// }
+// else if ( i == document.ul.length){
+// console.log("No more bottles of coke on the wall, no more bottles of coke. Go to the store and buy some more, 99 bottles of coke on the wall.");
+// }
+// else {
+
+// }
+// }
+
+
+
+
+
+
+
+// var bott = document.getElementById(songlist);
+// var x = "bottles";
+// var num = 99;
+// while (num > 1) {
+//  console.log(num + " " + x + " of coke on the wall, "+num + " " + x + " of coke,"+"Take one down and pass it around,"+ (num-1) + " " + x + " of coke on the wall.");
+//  num = num - 1;
+//  if (num > 0) {
+
+//  }
+
+//  else {
+//  var x3 = ("No more " + x + " of coke on the wall.");
+// }
+// var x1 = "bottle";
+// if (num <= 1) 
+// {
+//  var x2 = (num + " " + x1 + " of coke on the wall, "+num + " " + x1 + " of coke,"+"Take one down and pass it around,no more bottles of coke on the wall.");
+
+//  }
+// if (num == 0) 
+// {
+//  var x3 = ("No more bottles of coke on the wall, no more bottles of coke. Go to the store and buy some more"+ " 99 bottles of coke on the wall.");
+
+//  }
+
+// }
+// }
+
+
+// }
+// for (var i = 99 ; i >=0; i--)
+
+	  
+// {
+// if ( (i >0) && (i > 1)){
+// 	var songlist0 = (i +" bottles of coke on the wall, "+ i+ " bottles of coke. Take one down and pass it around, "+ (i-1) +" bottles of coke on the wall.");
+// }
+// else if ( (i > 0) && (i == 1)){
+// var songlist1 = (i +" bottle of coke on the wall, " + i + " bottle of coke. Take one down and pass it around, no more bottles of coke on the wall.");
+// }
+// else if ( i == 0){
+// var songlist2 = ("No more bottles of coke on the wall, no more bottles of coke. Go to the store and buy some more, 99 bottles of coke on the wall.");
+// }
