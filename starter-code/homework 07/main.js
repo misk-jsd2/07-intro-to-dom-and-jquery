5f31db2d4d6a76a2541d6803031e53038456669a
Ziyad S <ziyads.mansouri@gmail.com>
2019-04-08 20:27:00 +0300

    ziyad

--- starter-code/homework 07/main.js
+++ starter-code/homework 07/main.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
 for (var i=99;i>=0;i--){
 var $newThingLi = $('<li>');
 var $list= $('#Slist'); 
-
+var num=i;
 if (i==1){
-newThingLi.text("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall");
+$newThingLi.text("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall");
 $list.append($newThingLi);
 }else if (i==0){
-	newThingLi.text("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall");
+	$newThingLi.text("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall");
 $list.append($newThingLi);
 }else 
-newThingLi.text(i,"bottles of beer on the wall",i, "bottles of beer Take one down and pass it around", --i ,"bottles of beer on the wall")
+$newThingLi.text(num+"bottles of beer on the wall"+num+ "bottles of beer Take one down and pass it around"+ --num +"bottles of beer on the wall")
 $list.append($newThingLi);
 
 
